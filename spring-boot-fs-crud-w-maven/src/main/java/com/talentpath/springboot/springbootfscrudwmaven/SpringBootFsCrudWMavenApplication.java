0eccb1adf5646c2f182f646d025898f95e9c4d02
JC <avalojc@gmail.com>
2020-06-23 15:59:12 -0400

    bootstrap react and springboot

---
+++ spring-boot-fs-crud-w-maven/src/main/java/com/talentpath/springboot/springbootfscrudwmaven/SpringBootFsCrudWMavenApplication.java
@@ -0,0 +1,13 @@
+package com.talentpath.springboot.springbootfscrudwmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFsCrudWMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFsCrudWMavenApplication.class, args);
+	}
+
+}
---
+++ spring-boot-fs-crud-w-maven/src/test/java/com/talentpath/springboot/springbootfscrudwmaven/SpringBootFsCrudWMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talentpath.springboot.springbootfscrudwmaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFsCrudWMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
