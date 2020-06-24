48922627745b81791462a44fd5aad6a4778c67b2
JC <avalojc@gmail.com>
2020-06-24 15:06:31 -0400

    created service, added find all, save, delete, and find methods

---
+++ spring-boot-fs-crud-w-maven/src/main/java/com/talentpath/springboot/springbootfscrudwmaven/course/CourseService.java
@@ -0,0 +1,58 @@
+package com.talentpath.springboot.springbootfscrudwmaven.course;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CourseService {
+
+    private static List<Course> courses = new ArrayList<>();
+    private static long idCounter = 0;
+
+    static { // hardcoded courses
+        courses.add(new Course(++idCounter, "Biology 101", "BIOL101", "First Semester Introductory Course to Biology"));
+        courses.add(new Course(++idCounter, "Chemistry 102", "CHEM102", "Second Semester Introductory Course to Chemistry"));
+        courses.add(new Course(++idCounter, "English 101", "ENGL101", "First Semester Introductory Course to English"));
+        courses.add(new Course(++idCounter, "Calculus 101", "MATH301", "First Semester Introductory Course to Calculus"));
+    }
+
+    public List<Course> findAll() { //Show all courses
+        return courses;
+    }
+
+    public Course save(Course course){
+        if (course.getId() <= 0) {
+            course.setId(++idCounter);
+            courses.add(course);
+        } else {
+            deleteById(course.getId());
+            courses.add(course);
+        }
+        return course;
+    }
+
+    public Course deleteById(long id) {
+        Course course = findById(id);
+
+        if (course == null)
+            return null;
+
+        if (courses.remove(course)) {
+            return course;
+        }
+
+        return null;
+    }
+
+    public Course findById(long id) {
+        for (Course course : courses) {
+            if (course.getId() == id) {
+                return course;
+            }
+        }
+
+        return null;
+    }
+}
